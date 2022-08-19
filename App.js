b19f6293b3000ee26b55efdca05d8e195b80e605
SebastianRosenquist <seb_rosenquist@hotmail.com>
2022-08-19 20:47:26 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {createStackNavigator} from "@react-navigation/stack";
+//import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import CameraScreen from "./components/CameraScreen";
+import {NavigationContainer} from "@react-navigation/native";
+//import Ionicons from "react-native-vector-icons/Ionicons";
+import ImageScreen from "./components/ImageScreen";
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const Stack = createStackNavigator();
+  /*Bruger stack navigator til at vise billeder taget fra app*/
+  return(
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name={'home'} component={CameraScreen} options={{headerShown: false}} />
+          <Stack.Screen name={'image'} component={ImageScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
+/*const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});*/
